dd45ae945432333bd39e73c21330b8122336ed8f
ArunRajaSK <arunjune94@gmail.com>
2017-07-11 08:30:38 +0530

    testing

--- src/main/java/com/example/arunraja/games/Ground.java
+++ src/main/java/com/example/arunraja/games/Ground.java
@@ -4,10 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Ground extends AppCompatActivity {
-
+String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_ground);
     }
 }
